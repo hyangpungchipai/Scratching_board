6410ae5a00d5239640bc58aad4a7bfe8b887290c
MiccoMeow <130149749+MiccoMeow@users.noreply.github.com>
2024-06-16 00:16:44 +0800

    Create rebar_and_spam_and_reason.pl

---
+++ rebar_and_spam_and_reason.pl
@@ -0,0 +1,19 @@
+# rebar_and_spam_and_reason.pl
+
+#!/usr/bin/perl
+use constant THEY => "unmodifiable";
+unless ( $reason_known==1 ) {
+    my $meaning = 0;
+}
+my $date_known = 0;
+my $status_known = 1;
+my $count_dead_body = 1;
+if ( $continueable == 1 ) {
+   print "I do not know.\n";
+}
+else{
+  print "have sth to do with.\n";
+}
+@hard_working_people = qw/aunt1 aunt2 aunt3 aunt4 uncle1 uncle2 mother/;
+my $count_daoshi_item = 1;
+my $count_unknown_species =1;
